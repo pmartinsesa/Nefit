916b2708e1f9bc076f26c221c8d108c4f5863678
pmartinsesa <pmartinsesa@gmail.com>
2024-09-28 18:26:35 -0300

    chore: add notfound page and finish news

---
+++ src/app/not-found.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import logo from "../Assets/images/logo-nefit-footer.png";
+import Image from "next/image";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <div className="absolute bg-slate-200 w-screen h-screen z-20 flex justify-center items-center text-gray-700">
+        <div className="flex flex-col justify-center items-center">
+          <Image
+            className="grayscale object-fill h-[3.75rem] w-[10.65rem] bg-no-repeat"
+            src={logo}
+            alt="logo"
+          />
+          <div className="font-bold">Opa!</div>
+          <div>Por enquanto, não tem nada por aqui...</div>
+          <button
+            className="
+            justify-self-center col-span-12 
+            transition duration-300 ease-in-out 
+            text-white bg-red-primary 
+            hover:text-red-primary hover:border-red-primary hover:bg-transparent    
+            px-4 py-1 border rounded-xl h-10 w-auto mt-12"
+            type="submit"
+          >
+            <Link className="text-lg" href="/">
+              Ir para a página inicial
+            </Link>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/publicacoes/[route]/page.tsx
+++ src/app/publicacoes/[route]/page.tsx
@@ -8,6 +8,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Loading } from "@/components/Loading";
 import { getMonth } from "@/app/utils/date";
 import { AsideNews } from "@/components/publicacoes/AsideNews";
+import { redirect } from "next/navigation";
 
 export type NewsDetailsProps = {
   params: { route: string };
@@ -41,46 +42,80 @@ export default function NewsDetails({ params }: NewsDetailsProps) {
         setNewsList(newsList);
         setNews(newsList.filter((n) => n.route === params.route)[0]);
       })
-      .then(() => setIsLoading(false));
+      .then(() => setIsLoading(false))
+      .catch(() => {
+        redirect("/not-found");
+      });
   }, []);
 
-  return isLoading ? (
-    <ChakraProvider>
-      <Loading />
-    </ChakraProvider>
-  ) : (
-    <>
-      <Cover />
-      <div className=" flex justify-center">
-        <div className="grid grid-cols-12 my-14 min-[1950px]:w-[1950px]">
-          <div className="col-start-2 col-span-6 mt-8 min-[1950px]:justify-self-center">
-            <div className=" text-red-primary text-4xl font-bold max-[1270px]:mt-10 ">
-              {news.title}
-            </div>
-            <div className="text-gray-500 mt-4 text-lg font-light">
-              {`Postado em ${new Date(news.postedAt)
-                .getDay()
-                .toString()
-                .padStart(2, "0")} de ${getMonth(
-                new Date(news.postedAt)
-              )} de ${new Date(news.postedAt).getFullYear()}`}
-            </div>
-          </div>
+  function linkfy(text: string) {
+    var urlRegex = /(https?:\/\/[^\s]+)/g;
+    return text.replace(
+      urlRegex,
+      '<a style="color: blue;" target="_blank" href="$1">$1</a>'
+    );
+  }
 
-          <div className="col-start-2 col-span-10 border-t-2 border-gray-300 mt-6 mb-16 w-full"></div>
+  const mapNewsContent = (content: string) => {
+    const split = content.split("\n");
 
-          <div className="col-start-2 col-span-10 ">
-            <div className="flex justify-between gap-12 flex-wrap max-[1550px]:justify-center">
-              <div className="text-gray-700 tracking-wider leading-relaxed w-[65%] max-[1550px]:w-full">
-                {news.content}
+    return split.map((p) => {
+      if (p.length <= 0) return <br />;
+      const linkifiedParagraph = linkfy(p);
+
+      return (
+        <>
+          <p
+            key={p}
+            dangerouslySetInnerHTML={{ __html: linkifiedParagraph }}
+          ></p>
+          <br />
+        </>
+      );
+    });
+  };
+
+  try {
+    return isLoading ? (
+      <ChakraProvider>
+        <Loading />
+      </ChakraProvider>
+    ) : (
+      <>
+        <Cover />
+        <div className=" flex justify-center">
+          <div className="grid grid-cols-12 my-14 min-[1950px]:w-[1950px]">
+            <div className="col-start-2 col-span-6 mt-8 min-[1950px]:justify-self-center">
+              <div className=" text-red-primary text-4xl font-bold max-[1270px]:mt-10 ">
+                {news.title}
               </div>
-              <div className="">
-                <AsideNews newsList={newsList} />
+              <div className="text-gray-500 mt-4 text-lg font-light">
+                {`Postado em ${new Date(news.postedAt)
+                  .getDay()
+                  .toString()
+                  .padStart(2, "0")} de ${getMonth(
+                  new Date(news.postedAt)
+                )} de ${new Date(news.postedAt).getFullYear()}`}
+              </div>
+            </div>
+
+            <div className="col-start-2 col-span-10 border-t-2 border-gray-300 mt-6 mb-16 w-full"></div>
+
+            <div className="col-start-2 col-span-10 ">
+              <div className="flex justify-between gap-12 flex-wrap max-[1550px]:justify-center">
+                <div className="text-gray-700 tracking-wider leading-relaxed w-[65%] max-[1550px]:w-full">
+                  {mapNewsContent(news.content)}
+                </div>
+                <div className="">
+                  <AsideNews newsList={newsList} />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } catch (e) {
+    redirect("/not-found");
+  }
 }
