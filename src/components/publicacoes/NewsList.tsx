d9fe427d088a0629655bd6c8e309f42ef7f4f120
pmartinsesa <pmartinsesa@gmail.com>
2025-06-13 12:18:13 -0300

    fix: remove unnecessary class from NewsList component

--- src/components/publicacoes/NewsList.tsx
+++ src/components/publicacoes/NewsList.tsx
@@ -11,7 +11,7 @@ export const NewsList = ({ news }: NewsListProps) => {
     <>
       <div className="grid grid-cols-12 my-14">
         <div className="col-start-2 col-span-10 mt-8 min-[1950px]:justify-self-center">
-          <div className="grid grid-cols-1 justify-items-center gap-y-24 min-[1270px]:grid-cols-2 min-[1270px]:justify-items-start">
+          <div className="grid grid-cols-1 gap-y-24 min-[1270px]:grid-cols-2 min-[1270px]:justify-items-start">
             {news.map((n) => {
               return (
                 <PublishCard
